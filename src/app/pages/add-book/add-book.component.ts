9773e8bf5d333b8acb549efd87955bf07f6038ff
Miguel Franco <francamente@hotmail.es>
2023-11-10 12:17:03 +0100

    reto 1

---
+++ src/app/pages/add-book/add-book.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-book',
+  templateUrl: './add-book.component.html',
+  styleUrls: ['./add-book.component.css']
+})
+export class AddBookComponent {
+
+  public anyade(titulo, genero, autor, precio, url, idLibro,){}
+
+}
